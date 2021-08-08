a9a4a233bc8a28173cb13d6c2b9fa7b86bc5628f
Jay Harkar <jayshekar@gmail.com>
2021-08-07 22:35:15 -0500

    first save

--- src/main/java/com/jh/practice/leetcode/LeetcodeApplication.java
+++ src/main/java/com/jh/practice/leetcode/LeetcodeApplication.java
@@ -1,13 +1,27 @@
 package com.jh.practice.leetcode;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jh.practice.leetcode.recursion.MergeSort;
 
 @SpringBootApplication
 public class LeetcodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeetcodeApplication.class, args);
+		int[] mergeSort = MergeSort.sort(new int[]{1,5,3,2, 8,7,6,4});
+		System.out.println(Arrays.toString(mergeSort));		
 	}
-
+	
+//	void selectTopic() {
+//		switch()
+//	}
+//	
+//	void selectSubtopic() {
+//		
+//	}
+	
 }
--- src/main/java/com/jh/practice/leetcode/recursion/MergeSort.java
+++ src/main/java/com/jh/practice/leetcode/recursion/MergeSort.java
@@ -1,8 +1,38 @@
 package com.jh.practice.leetcode.recursion;
 
+import java.util.Arrays;
+
 public class MergeSort {
 	public static int[] sort(int[] array) {
-		return null;
+		if (array.length <= 1) {
+			return array;
+		}
+		
+		int mid = array.length / 2;
+		int[] left = sort(Arrays.copyOfRange(array, 0, mid));
+		int[] right = sort(Arrays.copyOfRange(array, mid, array.length));
+		
+		return merge(left, right);
+	}
+	
+	private static int[] merge(int[] left, int[] right) {
+		int leftCursor = 0, rightCursor = 0;
+		
+		int[] output = new int[left.length + right.length];
+		int index = 0;
+		while (leftCursor < left.length && rightCursor < right.length) {
+			output[index++] =  left[leftCursor] < right[rightCursor] ?
+				left[leftCursor++] : right[rightCursor++];
+		}
+		
+		while (leftCursor < left.length) {
+			output[index++] = left[leftCursor++]; 
+		}
+		while (rightCursor < right.length) {
+			output[index++] = right[rightCursor++];
+		}
+		
+		return output;
 	}
 
 }
